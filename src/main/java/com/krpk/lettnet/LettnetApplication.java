f07e17628831490b36f3d2138a75648d98dd4e7d
Krupko Bohdan <krupko98@gmail.com>
2019-07-17 22:06:37 +0300

    First commit

---
+++ src/main/java/com/krpk/lettnet/LettnetApplication.java
@@ -0,0 +1,13 @@
+package com.krpk.lettnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettnetApplication.class, args);
+	}
+
+}
